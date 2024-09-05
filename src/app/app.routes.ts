71cba0ebe349927a0790e535be9934f088204ec8
pablogonzamad <pa.gonzalezm@duocuc.cl>
2024-09-05 09:31:32 -0400

    Estructura Lista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'perfil', component: PerfilComponent},
+    {path: 'ajustes', component: SettingsComponent},
     {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
