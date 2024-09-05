34714697abc4f9988d83b8e8261d6aefb6c0e5f4
pablogonzamad <pa.gonzalezm@duocuc.cl>
2024-09-05 11:59:37 -0400

    Ajustes y Perfil Estructura Lista

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
