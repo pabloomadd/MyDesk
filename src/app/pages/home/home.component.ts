97dccf89c316f2b631a9dfbd5385ccbbb34d3cf2
pablogonzamad <pa.gonzalezm@duocuc.cl>
2024-09-06 10:50:38 -0400

    Reloj con Zona Horaria

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,21 +8,68 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  fecha = new Date();
-  hours?: number;
-  minutes?: number;
-  secs?: number;
+  @ViewChild('hrHand', { static: false }) hrHand!: ElementRef;
+  @ViewChild('minHand', { static: false }) minHand!: ElementRef;
+  @ViewChild('secHand', { static: false }) secHand!: ElementRef;
 
-  ngOnInit(): void {
+  secToDeg?: number;
+  minToDeg?: number;
+  hrToDeg?: number;
+
+  private intervalId?: ReturnType<typeof setInterval>;
+
+  constructor() {
   }
 
-  updateTime() {
-    // OBTENER TIEMPO ACTUAL Y CALCULAR GRADOS PARA MANECILLAS
-    this.secs = this.fecha.getSeconds();
-    this.minutes = this.fecha.getMinutes();
-    this.hours = this.fecha.getHours();
+  ngOnInit(): void {
+    this.intervalId = setInterval(() => {
+      const fecha = this.getTimeInTimeZone('America/Santiago');
+      this.updateTime(fecha);
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId); // Limpia el intervalo cuando el componente se destruye
+    }
+  }
+
+  updateTime(date: Date) {
+
+    this.secToDeg = (date.getSeconds() / 60) * 360;
+    this.minToDeg = (date.getMinutes() / 60) * 360;
+    this.hrToDeg = (date.getHours() / 12) * 360;
+
+
+    // ROTAR SECUNDERO SEGUN TIEMPO ACTUAL
+    this.secHand.nativeElement.style.transform = `rotate(${this.secToDeg}deg)`
+    this.minHand.nativeElement.style.transform = `rotate(${this.minToDeg}deg)`
+    this.hrHand.nativeElement.style.transform = `rotate(${this.hrToDeg}deg)`
+
+  }
+
+  // OBTENER ZONA HORARIA
+  getTimeInTimeZone(timeZone: string): Date {
+    const timeString = new Intl.DateTimeFormat('en-US', {
+      timeZone,
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: false
+    }).formatToParts(new Date());
+
+    const hour = Number(timeString.find(part => part.type === 'hour')?.value);
+    const minute = Number(timeString.find(part => part.type === 'minute')?.value);
+    const second = Number(timeString.find(part => part.type === 'second')?.value);
+
+    const date = new Date();
+    date.setHours(hour);
+    date.setMinutes(minute);
+    date.setSeconds(second);
+
+    return date;
   }
 
 }
