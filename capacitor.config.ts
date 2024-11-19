29d57e368ba8daf6dd65c36e85afde2f21ad4b1d
pablogonzamad <pa.gonzalezm@duocuc.cl>
2024-11-19 19:26:23 -0300

    Ajustes Movil + Android App

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pabloomadd.dev',
+  appName: 'MyDesk',
+  webDir: 'dist/my-desk-angular/browser'
+};
+
+export default config;
